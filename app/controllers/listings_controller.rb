950350f14a1bef903b7bf1cd8a3e5118505d80ec
Nicholas Barkolias <nbarkolias@gmail.com>
2020-09-05 16:03:30 +1000

    listings controller

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,2 +1,41 @@
 class ListingsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index :show
+
+  def index
+    @listings = Listing.all
+  end
+
+  def show
+    @listing = Listing.find(params[:id])
+  end
+
+  def new
+    @listing = Listing.new
+  end
+
+  def create
+    @listing = Listing.new(listing_params)
+    @listing.save
+  end
+
+  def edit
+    @listing = Listing.find(params[:id])
+  end
+
+  def update
+    @listing = Listing.find(params[:id])
+    @listing = Listing.update(listing_params)
+  end
+
+  def destroy
+    @listing = Listing.find(params[:id])
+    @listing.destroy
+    redirect_to listings_path
+  end
+
+  private
+
+  def listing_params
+    params.require(:title, :description, :price, :user_id)
+  end
 end
