5cbf7fdaa407e1e0990cc3a67cb12d309689f1f9
Dulan Perera <dulanp18@gmail.com>
2020-09-08 22:23:56 +1200

    purchase routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :listings
+  resources :listings do
+    resources :purchases
+  end
+
 
 end
