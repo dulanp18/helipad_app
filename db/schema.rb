c4a11f5da5823c59569a6a0e7ee8532bfe459809
Nicholas Barkolias <nbarkolias@gmail.com>
2020-09-12 14:43:14 +1000

    review link

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_12_014121) do
+ActiveRecord::Schema.define(version: 2020_09_12_014148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,8 +55,8 @@ ActiveRecord::Schema.define(version: 2020_09_12_014121) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "review_id"
     t.string "status", default: "pending"
+    t.bigint "review_id"
     t.index ["listing_id"], name: "index_purchases_on_listing_id"
     t.index ["review_id"], name: "index_purchases_on_review_id"
     t.index ["user_id"], name: "index_purchases_on_user_id"
